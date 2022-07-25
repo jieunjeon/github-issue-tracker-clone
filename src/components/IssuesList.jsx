ca41a0b78779bf3ad98c11868a79b8e0279d5bd5
Jieun Jeon <jieunjeon1023@gmail.com>
2022-07-25 15:56:40 +0900

    update list

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { IssueItem } from "./IssueItem";
 
 export default function IssuesList() {
   const issuesQuery = useQuery(["issues"], () =>
